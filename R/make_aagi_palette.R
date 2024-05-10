2e887a52f7c959e80bcd2c9246caa1910defbee5
Adam H. Sparks <adam.sparks@curtin.edu.au>
2024-05-10 20:35:39 +0800

    Delete aagi_pal(), tidy make_aagi_palette()

--- R/make_aagi_palette.R
+++ R/make_aagi_palette.R
@@ -1,21 +1,16 @@
 
-
-
-
 #' Interpolate an AAGI Colour Palette
 #'
 #' This function takes a \acronym{AAGI} colour palette and generates more
-#'   colours from it, so that there are enough to make your chart.
-#'
-#' The interpolation method is set to `spline` (the default is `linear`) in an
+#'   colours from it, so that there are enough to make your chart. The
+#'   interpolation method is set to `spline` (the default is `linear`) in an
 #'   attempt to reduce the number of vomit colours that get produced when
 #'   generating many colours.
 #'
-#'
-#' @param palette (`character`; default = `graph`) given name of an
-#'   \acronym{AAGI} palette: [make_aagi_palettes()]
+#' @param palette (`character`; default = `colourful`) given name of an
+#'   \acronym{AAGI} palette: [aagi_palettes()]
 #' @param reverse (`Boolean`; default = `FALSE`) indicating if palette should
-#'   be reverse
+#'   be reversed.
 #' @param ... Additional arguments to pass to [grDevices::colorRampPalette()],
 #'   see details here [grDevices::colorRamp()].
 #'
@@ -27,20 +22,20 @@
 #' library(ggplot2)
 #' ggplot(mtcars, aes(x = mpg, y = hp, colour = as.character(wt))) +
 #'   geom_point() +
-#'   scale_colour_manual(values = make_aagi_palette()(29))
+#'   scale_colour_manual(values = make_aagi_palette())
 #'
 #' @export
-make_aagi_palette <- function(make_aagi_palette = c("colourful",
-                                                    "primary",
-                                                    "secondary",
-                                                    "sequential",
-                                                    "diverging"),
+#'
+make_aagi_palette <- function(aagi_palette = c("colourful",
+                                               "primary",
+                                               "secondary",
+                                               "sequential",
+                                               "diverging"),
                               reverse = FALSE,
                               ...) {
-  rlang::arg_match(make_aagi_palette)
+  rlang::arg_match(aagi_palette)
 
-
-  pal <- make_aagi_palette[[make_aagi_palette]]
+  pal <- aagi_palettes()[[aagi_palette]]
 
   if (reverse)
     pal <- rev(pal)
