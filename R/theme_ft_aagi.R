5bdd3be8844bf3f34cccb99b3a393402a5dceb94
Adam H. Sparks <adam.sparks@curtin.edu.au>
2024-07-09 15:47:34 +0800

    Namespace flextable::border

--- R/theme_ft_aagi.R
+++ R/theme_ft_aagi.R
@@ -1,12 +1,12 @@
 
 #' Apply AAGI Theme to a flextable Object
-#' 
+#'
 #' Apply theme AAGI to a \CRANpkg{flextable}.  An AAGI formatted table body is
 #'   grey with a teal header and white header font.  Header text is bold,
 #'   text columns are left aligned, other columns are right aligned.
-#' 
+#'
 #' # Behaviour
-#' 
+#'
 #' Theme functions are not like \CRANpkg{ggplot2} themes. They are applied to
 #'   the existing table **immediately**. If you add a row in the footer, the new
 #'   row is not formatted with the theme. The theme function applies the theme
@@ -26,7 +26,7 @@
 #'   `post_process_pptx`) to specify a theme to be applied systematically before
 #'   the `flextable()` is printed; in this case, don't forget to take care that
 #'   the theme doesn't override any formatting done before the print statement.
-#' 
+#'
 #' @param x a \CRANpkg{flextable} object
 #' @return a formatted \CRANpkg{flextable} object
 #' @examples
@@ -54,7 +54,7 @@ theme_ft_aagi <- function(x) {
   x <- flextable::color(x = x, color = "#414042", part = "body")
   x <- flextable::bg(x = x, bg = "#F2F2F2", part = "body")
 
-  x <- border(
+  x <- flextable::border(
     x = x, border = officer::fp_border(
       width = 0.75,
       color = "#ffffff"
