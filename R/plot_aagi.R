16696bdb3810c953704746625b4d4c0052e4a319
Adam H. Sparks <adam.sparks@curtin.edu.au>
2026-05-05 17:25:45 +0800

    fix: this is the actual fix for plot_aagi() colours

--- R/plot_aagi.R
+++ R/plot_aagi.R
@@ -6,12 +6,10 @@
 #' @param x the coordinates of points in the plot. Alternatively, a single
 #'  plotting structure, function or any \R _object with a plot method_ can be
 #'  provided.
-#' @param col Colour to use as fill for bars  Defaults to a very dark grey,
-#'  *i.e.*, "AAGI Black".
 #' @param ... Arguments to be passed to methods, such as graphical parameters
 #'   (see [graphics::par()]).  The most commonly used argument would be
-#'   `y` for the factor to use for the y-axis or `type` describing what type of
-#'   plot should be drawn.
+#'   `y` for the factor to use for the y-axis, `type` describing what type of
+#'   plot should be drawn, or `col` for point colour (defaults to AAGI Black).
 #'
 #' @seealso [graphics::plot()] for full documentation of the basic plotting
 #'  capabilities.
@@ -30,14 +28,33 @@
 #' \acronym{AAGI} style.
 #' @export
 #'
-plot_aagi <- function(x, col = NULL, ...) {
-  # Set default colour and convert
-  col <- col %||% "AAGI Black"
-  col <- .convert_aagi_colour(col)
+plot_aagi <- function(x, ...) {
+  # Extract ... into a list to manipulate col
+  dots <- list(...)
+
+  # Handle col parameter: default to AAGI Black and convert
+  if (is.null(dots$col)) {
+    dots$col <- "AAGI Black"
+  }
+
+  # Validate col is scalar character
+  if (!rlang::is_scalar_character(dots$col)) {
+    dots$col <- "AAGI Black"
+  }
+
+  # Convert AAGI colour names to hex
+  dots$col <- .convert_aagi_colour(dots$col)
 
   withr::local_par(par_aagi())
   showtext::showtext_begin()
   on.exit(showtext::showtext_end(), add = TRUE)
 
-  graphics::plot(x, col = col, ...)
+  # Use do.call with explicit col handling for formula support
+  if (rlang::is_formula(x)) {
+    # For formula interface, construct the call carefully
+    graphics::plot(x, col = dots$col, ...)
+  } else {
+    # For regular interface, use do.call
+    do.call(graphics::plot, c(list(x = x), dots))
+  }
 }
