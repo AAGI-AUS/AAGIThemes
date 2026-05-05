ffa55fb4fcc52568364e5c38cc5fc70713c9896b
Adam H. Sparks <adam.sparks@curtin.edu.au>
2026-05-05 15:03:52 +0800

    fix: clean up `pch` description and xref the documentation for `par`

--- R/boxplot_aagi.R
+++ R/boxplot_aagi.R
@@ -14,23 +14,15 @@
 #' @param ylab Y-axis label.  Optional.
 #' @param col Colour to use as fill for boxes  Defaults to white.  Can be
 #'   supplied as a named AAGI colour, *e.g.*, "AAGI Orange"; named colour,
-#'   "Orange"; or or a hexadecimal code, "#ec8525".
-#' @param pch plotting ‘character’, \emph{i.e.}, symbol to use. This can either
-#'   be a single character or an integer code for one of a set of graphics
-#'   symbols. The full set of S symbols is available with `pch = 0:18`, see the
-#'   examples below. (NB: \R uses circles instead of the octagons used in S.)
-#'   Value `pch = "."` (equivalently `pch = 46`) is handled specially. It is a
-#'   rectangle of side 0.01 inch (scaled by `cex`). In addition, if `cex = 1`
-#'   (the default), each side is at least one pixel (1/72 inch on the
-#'   [grDevices::pdf()] and [grDevices::postscript()] devices).
-#'   For other text symbols, `cex = 1` corresponds to the default font size of
-#'   the device, often specified by an argument `pointsize`. For `pch` in 0:25
-#'   the default size is about 75% of the character height (see `par("cin")`).
+#'   "Orange"; or or a hexadecimal code, *e.g.*, "#ec8525".
+#' @param pch plotting ‘character’, \emph{i.e.}, symbol to use.
 #' @param ... Arguments to be passed to methods, such as graphical parameters
 #'   (see [graphics::par()]).
 #'
-#' @seealso [graphics::boxplot()] for full documentation of the basic boxplot
-#'    capabilities
+#' @seealso
+#'  * [graphics::boxplot()] for full documentation of the basic boxplot
+#'    capabilities, and
+#'  * [graphics::par()] for full documentation of `pch`.
 #' @examples
 #' boxplot_aagi(decrease ~ treatment,
 #'   data = OrchardSprays,
