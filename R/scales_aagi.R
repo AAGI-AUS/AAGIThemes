ae2988d508a09ab498fc8dc0ec220c10228b6fda
Adam H. Sparks <adam.sparks@curtin.edu.au>
2026-05-12 12:39:15 +0800

    fix: scales_aagi functions now allow use of named AAGI colours and arrange colours for better data vis

--- R/scales_aagi.R
+++ R/scales_aagi.R
@@ -1,4 +1,4 @@
-#' Scale constructors for AAGI colour palettes
+#' Scale constructors for AAGI colour palettes to be used in {ggplot2} objects
 #'
 #' Helpers for applying \acronym{AAGI} colour palettes to \CRANpkg{ggplot2}
 #' colour and fill aesthetics.
@@ -14,58 +14,49 @@
 #'   [AAGIPalettes::aagi_palettes()]. If `NULL` and `discrete = TRUE`, use the
 #'   official colours in [AAGIPalettes::aagi_colours].
 #' @param discrete Logical; should the scale be discrete? Defaults to `TRUE`.
-#' @param reverse Logical; should the palette order be reversed? Defaults to
-#'   `FALSE`.
 #' @param colours Character vector of official \acronym{AAGI} colour names to
 #'   interpolate for continuous scales. If `NULL`, defaults used by
 #'   [AAGIPalettes::interpolate_aagi_colours()] are applied.
+#' @param values Optional character vector of colour values to use for discrete
+#'   scales. This can be a vector of hex colours or values returned by
+#'   [AAGIPalettes::colour_as_hex()]. If supplied, `values` takes precedence over
+#'   `palette` and the default ordering of [AAGIPalettes::aagi_colours].
 #' @param ... Additional arguments passed to the underlying \CRANpkg{ggplot2}
 #'   scale functions.
-#'
-#' @details
-#' For discrete scales:
-#' - if `palette = NULL`, colours are taken from [AAGIPalettes::aagi_colours];
-#' - otherwise colours are taken from [AAGIPalettes::aagi_palettes()].
-#'
-#' For continuous scales, `palette` is ignored and interpolated colours are
-#' used. If you want to control the continuous gradient directly, supply
-#' `colours`.
-#'
-#' @examples
-#' p <- ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(displ, hwy, colour = class)) +
-#'   ggplot2::geom_point()
-#'
-#' # Default discrete official colours
-#' p + scale_colour_aagi()
-#'
-#' # Named palette
-#' p + scale_colour_aagi(palette = "aagi_blues")
 #'
 #' @describeIn scale_colour_aagi For colour scales
 #' @export
 scale_colour_aagi <- function(
   palette = NULL,
   discrete = TRUE,
-  reverse = FALSE,
   colours = NULL,
+  values = NULL,
   ...
 ) {
-  direction <- if (isTRUE(reverse)) -1 else 1
-
   if (isTRUE(discrete)) {
     pal <- function(n) {
-      if (is.null(palette)) {
-        vals <- unname(AAGIPalettes::aagi_colours[.aagi_colour_order()])
+      if (!is.null(values)) {
+        vals <- unname(values)
 
-        if (direction == -1) {
-          vals <- rev(vals)
+        if (n > length(vals)) {
+          cli::cli_abort(
+            c(
+              "x" = "Not enough values supplied in {.arg values} for {.val {n}} levels."
+            )
+          )
         }
+
+        return(vals[seq_len(n)])
+      }
+
+      if (is.null(palette)) {
+        vals <- unname(AAGIPalettes::aagi_colours[.aagi_default_colour_order()])
 
         if (n > length(vals)) {
           cli::cli_abort(
             c(
               "x" = "Not enough values in {.field AAGIPalettes::aagi_colours} for {.val {n}} levels.",
-              "i" = "Supply a named {.arg palette} or use {.code discrete = FALSE}."
+              "i" = "Supply {.arg values}, a named {.arg palette}, or use {.code discrete = FALSE}."
             )
           )
         }
@@ -74,8 +65,7 @@ scale_colour_aagi <- function(
       } else {
         AAGIPalettes::aagi_palettes(
           name = palette,
-          n = n,
-          direction = direction
+          n = n
         )
       }
     }
@@ -87,12 +77,9 @@ scale_colour_aagi <- function(
     )
   } else {
     pal <- if (is.null(colours)) {
-      AAGIPalettes::interpolate_aagi_colours(direction = direction)
+      AAGIPalettes::interpolate_aagi_colours()
     } else {
-      AAGIPalettes::interpolate_aagi_colours(
-        colours = colours,
-        direction = direction
-      )
+      AAGIPalettes::interpolate_aagi_colours(colours = colours)
     }
 
     ggplot2::scale_colour_gradientn(
@@ -107,26 +94,34 @@ scale_colour_aagi <- function(
 scale_fill_aagi <- function(
   palette = NULL,
   discrete = TRUE,
-  reverse = FALSE,
   colours = NULL,
+  values = NULL,
   ...
 ) {
-  direction <- if (isTRUE(reverse)) -1 else 1
-
   if (isTRUE(discrete)) {
     pal <- function(n) {
-      if (is.null(palette)) {
-        vals <- unname(AAGIPalettes::aagi_colours[.aagi_colour_order()])
+      if (!is.null(values)) {
+        vals <- unname(values)
 
-        if (direction == -1) {
-          vals <- rev(vals)
+        if (n > length(vals)) {
+          cli::cli_abort(
+            c(
+              "x" = "Not enough values supplied in {.arg values} for {.val {n}} levels."
+            )
+          )
         }
+
+        return(vals[seq_len(n)])
+      }
+
+      if (is.null(palette)) {
+        vals <- unname(AAGIPalettes::aagi_colours[.aagi_default_colour_order()])
 
         if (n > length(vals)) {
           cli::cli_abort(
             c(
               "x" = "Not enough values in {.field AAGIPalettes::aagi_colours} for {.val {n}} levels.",
-              "i" = "Supply a named {.arg palette} or use {.code discrete = FALSE}."
+              "i" = "Supply {.arg values}, a named {.arg palette}, or use {.code discrete = FALSE}."
             )
           )
         }
@@ -135,8 +130,7 @@ scale_fill_aagi <- function(
       } else {
         AAGIPalettes::aagi_palettes(
           name = palette,
-          n = n,
-          direction = direction
+          n = n
         )
       }
     }
@@ -148,12 +142,9 @@ scale_fill_aagi <- function(
     )
   } else {
     pal <- if (is.null(colours)) {
-      AAGIPalettes::interpolate_aagi_colours(direction = direction)
+      AAGIPalettes::interpolate_aagi_colours()
     } else {
-      AAGIPalettes::interpolate_aagi_colours(
-        colours = colours,
-        direction = direction
-      )
+      AAGIPalettes::interpolate_aagi_colours(colours = colours)
     }
 
     ggplot2::scale_fill_gradientn(
@@ -163,21 +154,15 @@ scale_fill_aagi <- function(
   }
 }
 
-#' Set a different default order of AAGI colours for better data vis
-#'
-#' Sets the colour order so that there is more contrast between the first three
-#' colours since graphs frequently only have 1-3 values displayed.
-#'
-#' @dev
-.aagi_colour_order <- function() {
+.aagi_default_colour_order <- function() {
   c(
+    "AAGI Teal",
+    "AAGI Blue",
     "AAGI Bright Green",
     "AAGI Orange",
-    "AAGI Blue",
-    "AAGI Green",
     "AAGI Yellow",
+    "AAGI Green",
     "AAGI Black",
-    "AAGI Teal",
     "AAGI Grey"
   )
 }
